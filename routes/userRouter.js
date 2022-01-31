0bd39067913708327e7b923d1c9e86f8a393d7a2
DungAF <nt.dung@afterfit.co.jp>
2022-01-31 11:33:14 +0700

    config auth 2

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,7 +4,24 @@ const auth = require('../middleware/auth')
 
 router.post('/register', userCtrl.register)
 
+router.post('/activation', userCtrl.activateEmail)
+
 router.post('/login', userCtrl.login)
+
+router.post('/refresh_token', userCtrl.getAccessToken)
+
+router.post('/forgot', userCtrl.forgotPassword)
+
+router.post('/reset', auth, userCtrl.resetPassword)
+
+router.post('/change_password', auth, userCtrl.changePassword)
+
+// Social Login
+router.post('/google_login', userCtrl.googleLogin)
+
+router.post('/facebook_login', userCtrl.facebookLogin)
+
+
 
 router.get('/logout', userCtrl.logout)
 
